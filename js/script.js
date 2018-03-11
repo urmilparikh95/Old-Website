f54224f9565b7bd290cb81a67e172cabf3b42936
Urmil Parikh <uparikh@ncsu.edu>
2018-03-11 13:59:50 -0400

    bug fixes

--- js/script.js
+++ js/script.js
@@ -2,27 +2,27 @@ $(document).ready(function () {
   var bg = $('#home');
   var i = 1;
 
-  // setInterval(function () {
+  setInterval(function () {
 
-  //   bg.fadeOut(2000, function () {
-  //     bg.css({
-  //       "height": "100vh",
-  //       "background": "url(img/b0" + i + ".jpg)  no-repeat",
-  //       "-webkit-background-size": "cover",
-  //       "-moz-background-size": "cover",
-  //       "-o-background-size": "cover",
-  //       "background-size": "cover",
-  //       "background-position": "center"
-  //     });
-  //     bg.fadeIn(2000);
-  //   });
+    bg.fadeOut(2000, function () {
+      bg.css({
+        "height": "100vh",
+        "background": "url(img/b0" + i + ".jpg)  no-repeat",
+        "-webkit-background-size": "cover",
+        "-moz-background-size": "cover",
+        "-o-background-size": "cover",
+        "background-size": "cover",
+        "background-position": "center"
+      });
+      bg.fadeIn(2000);
+    });
 
-  //   i = i + 1;
-  //   if (i > 7) {
-  //     i = 1;
-  //   }
+    i = i + 1;
+    if (i > 7) {
+      i = 1;
+    }
 
-  // }, 250000);
+  }, 250000);
 
   $('#about').click(function () {
     $('.tab-content').removeClass('current');
@@ -49,9 +49,9 @@ $(document).ready(function () {
 
   var d = new Date();
   if (d.getHours() < 10) {
-    var h = " 0" + d.getHours();
+    var h = "0" + d.getHours();
   } else {
-    var h = " " + d.getHours();
+    var h = "" + d.getHours();
   }
   if (d.getMinutes() < 10) {
     var m = "0" + d.getMinutes();
@@ -64,6 +64,7 @@ $(document).ready(function () {
       content = "<" + h + ":" + m + ">";
       len = content.length;
     } else {
+      h = " " + h;
       content = h + ":" + m + " <root@" + response.ip + ">";
       len = content.length + 3;
     }
@@ -75,7 +76,7 @@ $(document).ready(function () {
 
 new TypeIt('.shell-body', {
   // other options
-  speed: 22,
+  speed: 15,
   cursorChar: '&#9608;',
   callback: function () {
     $('.project').click(function () {
